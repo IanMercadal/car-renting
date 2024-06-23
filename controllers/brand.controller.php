0abcc59e7ba1a822d692fadbdbb1bea71011b45c
Ian Mercadal <ianmercadalf02@gmail.com>
2024-06-23 20:11:31 +0200

    feat: Creating controller/model of brands
    Creating files, properties and methods

---
+++ controllers/brand.controller.php
@@ -0,0 +1,76 @@
+<?php 
+
+class BrandController {
+    
+    public function index($data) : array {
+        $result = array("status" => 500,"content" => "Couldn't find brands");
+
+        $brand = new Brand();
+        $request = $brand->index();
+
+        if($request["content"]) {
+            $result["status"] = 200;
+            $result["content"] = $request["content"];
+        }
+
+        return $result;
+    }
+
+    public function show($data) : array {
+        $result = array("status" => 500,"content" => "Couldn't find brand");
+
+        $brand = new Brand($data["brand_id"],NULL,NULL);
+        $request = $brand->show();
+
+        if($request["content"]) {
+            $result["status"] = 200;
+            $result["content"] = $request["content"];
+        }
+
+        return $result;
+    }
+
+    public function create($data) : array {
+        $result = array("status" => 500,"content" => "Couldn't create brand");
+
+        $brand = new Brand(NULL,$data["name"],$data["active"]);
+        $request = $brand->create();
+
+        if($request) {
+            $result["status"] = 200;
+            $result["content"] = "Brand created successfully";
+        }
+
+        return $result;
+    }
+
+    public function update($data) : array {
+        $result = array("status" => 500,"content" => "Couldn't update brand");
+
+        $brand = new Brand($data["brand_id"],$data["name"],$data["active"]);
+        $request = $brand->update();
+
+        if($request) {
+            $result["status"] = 200;
+            $result["content"] = "Brand updated successfully";
+        }
+
+        return $result;
+    }
+
+    public function delete($data) : array {
+        $result = array("status" => 500,"content" => "Couldn't delete brand");
+
+        $brand = new Brand($data["brand_id"]);
+        $request = $brand->delete();
+
+        if($request) {
+            $result["status"] = 200;
+            $result["content"] = "Brand deleted successfully";
+        }
+
+        return $result;
+    }
+}
+
+?>
---
+++ models/brand.model.php
@@ -0,0 +1,112 @@
+<?php 
+
+class Brand {
+
+    private $brand_id;
+    private $name;
+    private $active;
+
+    function __construct($brand_id = NULL,$name = NULL,$active = NULL) {
+        $this->brand_id = $brand_id;
+        $this->name = $name;
+        $this->active = $active;
+    }
+
+    public function index() {
+        $result = array("content" => []);     
+        
+        $db = DB::getInstance();
+        $connection = $db->getConnection();
+
+        $query = "SELECT * FROM brands";
+        $request = $connection->query($query);
+
+        if ($request) {
+            while ($row = $request->fetch_assoc()) {
+                array_push($result["content"], $row);
+            }
+        }
+
+        return $result;
+    }
+
+    public function show() {
+        $result = array("content" => []);     
+        
+        $db = DB::getInstance();
+        $connection = $db->getConnection();
+
+        $query = "SELECT * FROM brands where brand_id = " . $this->brand_id;
+        $request = $connection->query($query);
+
+        if ($request) {
+            while ($row = $request->fetch_assoc()) {
+                array_push($result["content"], $row);
+            }
+        }
+
+        return $result;
+    }
+
+
+    public function create() : bool {
+        try {
+            $db = DB::getInstance();
+            $connection = $db->getConnection();
+            $connection->begin_transaction();
+
+            $query = "INSERT INTO brands (name,active) VALUES('".$this->name."',".$this->active.") ";
+            $request = $connection->query($query);
+
+            if (!$request) {
+                throw new Exception("Error creating brand");
+            }  
+            
+            return true;
+        } catch (Exception $e) {
+            $connection->rollback();
+            return false;
+        } 
+    }
+
+    public function update() : bool {
+        try {  
+            $db = DB::getInstance();
+            $connection = $db->getConnection();
+    
+            $query = "UPDATE brands SET name = '".$this->name."', active = '".$this->active."' where brand_id = ".$this->brand_id." ";
+    
+            $request = $connection->query($query);
+            if (!$request) {
+                throw new Exception("Error updating brand");
+            }  
+            
+            return true;
+        } catch (Exception $e) {
+            $connection->rollback();
+            return false;
+        }
+    }
+
+    public function delete() : bool {
+        try {
+            $db = DB::getInstance();
+            $connection = $db->getConnection();
+    
+            $query = "DELETE FROM brands WHERE brand_id = ".$this->brand_id;
+            $request = $connection->query($query);
+    
+            $request = $connection->query($query);
+            if (!$request) {
+                throw new Exception("Error deleting brand");
+            }  
+    
+            return true;
+        } catch (Exception $e) {
+            $connection->rollback();
+            return false;
+        }
+    }
+}
+
+?>
