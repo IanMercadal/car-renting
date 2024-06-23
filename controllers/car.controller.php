7a4561774ae42c7c026594557e41e210bea87f4d
Ian Mercadal <ianmercadalf02@gmail.com>
2024-06-23 17:21:53 +0200

    feat: Creating rest of functions for Car Controller with Standard data
    Creating basic functions with info from request

--- controllers/car.controller.php
+++ controllers/car.controller.php
@@ -2,13 +2,13 @@
 
 class CarController {
     
-    public function index() : array {
+    public function index($data) : array {
         $result = array(
             "status" => 0,
             "content" => ""
         );
 
-        $car = new Car();
+        $car = new Car($data["car_id"], $data["model"], $data["air_conditioner"], $data["shift"],$data["passengers"],$data["price"],$data["active"],$data["brand_id"]);
         $request = $car->index();
 
         if($request["content"]) {
@@ -19,6 +19,56 @@ class CarController {
         return $result;
     }
 
+    public function create($data) : array {
+        $result = array(
+            "status" => 0,
+            "content" => ""
+        );
+
+        $car = new Car($data["car_id"], $data["model"], $data["air_conditioner"], $data["shift"],$data["passengers"],$data["price"],$data["active"],$data["brand_id"]);
+        $request = $car->create();
+
+        if($request) {
+            $result["status"] = 200;
+            $result["content"] = "Car created successfully";
+        }
+
+        return $result;
+    }
+
+    public function update($data) : array {
+        $result = array(
+            "status" => 0,
+            "content" => ""
+        );
+
+        $car = new Car($data["car_id"], $data["model"], $data["air_conditioner"], $data["shift"],$data["passengers"],$data["price"],$data["active"],$data["brand_id"]);
+        $request = $car->update();
+
+        if($request["content"]) {
+            $result["status"] = 200;
+            $result["content"] = $request["content"];
+        }
+
+        return $result;
+    }
+
+    public function delete($data) : array {
+        $result = array(
+            "status" => 0,
+            "content" => ""
+        );
+
+        $car = new Car($data["car_id"], $data["model"], $data["air_conditioner"], $data["shift"],$data["passengers"],$data["price"],$data["active"],$data["brand_id"]);
+        $request = $car->delete();
+
+        if($request["content"]) {
+            $result["status"] = 200;
+            $result["content"] = $request["content"];
+        }
+
+        return $result;
+    }
 }
 
 ?>
