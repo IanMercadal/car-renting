b551516a92296abef992bc0f9aa8387c1cb341c4
Ian Mercadal <ianmercadalf02@gmail.com>
2024-07-07 12:27:41 +0200

    feat: New file for includes
    Creating new file to group all includes

--- ajax.php
+++ ajax.php
@@ -9,41 +9,7 @@
 // SET SPAIN HOUR
 date_default_timezone_set("Europe/Madrid");
 
-// USER
-require_once 'controllers/user.controller.php';
-require_once 'models/user.model.php';
-
-// CARS
-require_once 'controllers/car.controller.php';
-require_once 'models/car.model.php';
-
-// BRAND
-require_once 'controllers/brand.controller.php';
-require_once 'models/brand.model.php';
-
-// LOGS
-require_once 'controllers/logs.controller.php';
-require_once 'models/logs.model.php';
-
-// RESERVATION
-require_once 'controllers/reservation.controller.php';
-require_once 'models/reservation.model.php';
-
-// TOKEN
-require_once 'controllers/token.controller.php';
-require_once 'models/token.model.php';
-
-// DB
-require_once 'models/db.model.php';
-
-// AUTH
-require_once 'auth.php';
-
-// HELPERS
-require_once 'helpers.php';
-
-// HELPERS
-require_once 'host.php';
+include_once "./includes.php";
 
 // CHECKING WANTED ACTION/METHOD
 $available_actions = array("brand", "car", "reservation", "user");
